9b28a94c1db0e88b74a6168280cc43f070f43fd5
Didero <Didero@users.noreply.github.com>
2014-04-04 14:23:29 +0200

    Made uptime output much nicer

--- commands/uptime.py
+++ commands/uptime.py
@@ -1,4 +1,4 @@
-import time
+import datetime, time
 
 from CommandTemplate import CommandTemplate
 
@@ -8,8 +8,19 @@ class Command(CommandTemplate):
 	
 	def execute(self, bot, user, target, triggerInMsg, msg, msgWithoutFirstWord, msgParts, msgPartsLength):
 
-		uptime = time.time() - bot.connectedAt
+		uptime = round(time.time() - bot.connectedAt)
+		minutes, seconds = divmod(uptime, 60)
+		hours, minutes = divmod(minutes, 60)
+		days, hours = divmod(hours, 24)
 
-		replytext = u"I have been running for {:,} seconds".format(round(uptime, 1))
+		replytext = u"I have been running for "
+		if days > 0:
+			replytext += u"{:,.0f} days, ".format(days)
+		if hours > 0:
+			replytext += u"{:,.0f} hours, ".format(hours)
+		if minutes > 0:
+			replytext += u"{:,.0f} minutes, ".format(minutes)
+		replytext += u"{:,.0f} seconds".format(seconds)
+
 		bot.say(target, replytext)
 
