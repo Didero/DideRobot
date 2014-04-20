2250de7128b5e306227effe75740cb3f964bb3b8
Didero <Didero@users.noreply.github.com>
2014-04-20 21:44:51 +0200

    Made the ReloadSettings module work more sensibly, by having it reload only the current bot's settings unless the argument 'all' is provided

--- commands/reloadSettings.py
+++ commands/reloadSettings.py
@@ -7,6 +7,24 @@ class Command(CommandTemplate):
 	adminOnly = True
 
 	def execute(self, bot, user, target, triggerInMsg, msg, msgWithoutFirstWord, msgParts, msgPartsLength):
-		for serverfolder, botfactory in GlobalStore.bothandler.botfactories.iteritems():
-			botfactory.updateSettings()
-		bot.say(target, "Reloaded all settings")
+		replytext = u""
+		#If the keyword 'all' was provided, reload the settings of all bots
+		if msgWithoutFirstWord.lower() == "all":
+			serversWithReloadFault = []
+			for serverfolder, botfactory in GlobalStore.bothandler.botfactories.iteritems():
+				success = botfactory.updateSettings()
+				if not success:
+					serversWithReloadFault.append(serverfolder)
+
+			replytext = u"Reloaded all settings"
+			if len(serversWithReloadFault) > 0:
+				replytext += u" (error reloading settings for {})".format("; ".join(serversWithReloadFault))
+		#Otherwise, just reload the settings of this bot
+		else:
+			success = bot.factory.updateSettings()
+			if success:
+				replytext = u"Successfully reloaded settings for this bot"
+			else:
+				replytext = u"An error occurred while trying to reload the settings for this bot, check the debug output for the cause"
+				
+		bot.say(target, replytext)
