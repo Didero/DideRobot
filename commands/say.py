1567d8a63b362620a06b199b3f0406805631f3a4
Didero <Didero@users.noreply.github.com>
2014-05-25 13:37:20 +0200

    [Say] Fixed the check whether we're in the channel we're messaging

--- commands/say.py
+++ commands/say.py
@@ -15,7 +15,7 @@ class Command(CommandTemplate):
 		if message.messagePartsLength < 2:
 			message.bot.say(message.source, u"Please provide both a channel or user name to say something to, and the text to say")
 		#Check if we're in the channel we have to say something to
-		elif not message.isPrivateMessage and message.messageParts[0] not in message.bot.channelsUserList:
+		elif message.messageParts[0].startswith('#') and message.messageParts[0] not in message.bot.channelsUserList:
 			message.bot.say(message.source, u"I'm not in that channel, so I can't say anything in there, sorry.")
 		#Nothing's stopping us now! Say it!
 		else:
