fd305c436860532979e90335b2562605dfd7e99d
Didero <Didero@users.noreply.github.com>
2014-06-05 21:46:23 +0200

    [Humble] Allowed arbitrary page searches in case of special bundles besides normal and weekly ones
    [Humble] Fixed small encoding error with special game titles

--- commands/humblebundle.py
+++ commands/humblebundle.py
@@ -21,11 +21,11 @@ class Command(CommandTemplate):
 		gamenames = {}
 		page = None
 		title = u""
-		isWeekly = (message.message.lower() == 'weekly' or message.message.lower() == 'week')
 
 		url = "http://www.humblebundle.com/"
-		if isWeekly:
-			url += 'weekly'
+		#Allow for any special bundle search
+		if message.messagePartsLength > 0:
+			url += message.messageParts[0]
 
 		pagetext = requests.get(url).content
 		#BeautifulSoup doesn't handle non-standard newlines very well, it inserts </br> at the very end, messing up searching. Prevent that
@@ -34,21 +34,8 @@ class Command(CommandTemplate):
 		##pagetext = pagetext.replace("<!--", ">").replace("-->", ">")
 		page = BeautifulSoup(pagetext)
 
-		#Title is formatted like "Humble Weekly Sale: [company] (pay what...)"
-		if isWeekly:
-			titlematches = re.search("(.+) \(", page.title.string)
-			if not titlematches:
-				#print "No title found in '{}'".format(page.title.string)
-				title = "Humble Weekly Sale"
-			else:
-				title = "The {}".format(titlematches.group(1))
-		else:
-			titlematches = re.search("(.*) \(", page.title.string)
-			if not titlematches:
-				#print "No title found in '{}'".format(page.title.string)
-				title = "The current Humble Bundle"
-			else:
-				title = "The " + titlematches.group(1)
+		#Get the part of the title up to the first opening parenthesis, since that's where the 'Pay what you wan't message starts
+		title = page.title.string[:page.title.string.find('(') - 1]
 
 		gamecontainers = page.find_all(class_="game-boxes")
 		if len(gamecontainers) > 0:
@@ -129,61 +116,63 @@ class Command(CommandTemplate):
 					gamenames['BTA'].append(gamename)
 				#gamenames.append(gamename)
 
-
-		#Totals aren't shown on the site immediately, but are edited into the page with Javascript. Get info from there
-		totalMoney = -1.0
-		contributors = -1
-		avgPrice = -1.0
-		timeLeft = u""
-		for scriptElement in page.find_all('script'):
-			script = scriptElement.text
-			if script.count("'initial_stats_data':") > 0:
-				#This script element contains the initial data
-				match = re.search("'initial_stats_data':(.+),", script)
-				if match is None:
-					print "Expected to find initial values, but failed!"
-					print script
-				else:
-					data = json.loads(match.group(1))
-					if 'rawtotal' in data:
-						totalMoney = data['rawtotal']
-					else:
-						print "Sales data found, but total amount is missing!"
-						print data
-					if 'numberofcontributions' in data and 'total' in data['numberofcontributions']:
-						contributors = int(data['numberofcontributions']['total'])
-					else:
-						print "Contributor data not found!"
-						print data
-
-					if totalMoney > -1.0 and contributors > -1:
-						avgPrice = totalMoney / contributors
-
-					timeLeftMatch = re.search('var timing = \{"end": (\d+)\};', script)
-					if timeLeftMatch:
-						timeLeft = SharedFunctions.durationSecondsToText(int(timeLeftMatch.group(1)) - time.time())
-
-					break
-
-		if totalMoney == -1.0 or contributors == -1 or avgPrice == -1.0:
-			replytext = "Sorry, the data could not be retrieved. This is either because the site is down, or because of some weird bug. Please try again in a little while"
+		if len(gamenames) == 0:
+			replytext = u"No games found. Either an error occurred, or you tried to look up a non-existent bundle page."
 		else:
-			replytext = u"{title} has an average price of ${avgPrice:.2f} and raised ${totalMoney:,} from {contributors:,} people."
-			gamelist = u""
-			if timeLeft != u"":
-				replytext += u" It will end in {timeLeft}."
-			#If we didn't find any games, pretend like nothing's wrong
-			if len(gamenames) > 0:
-				replytext += u" It contains {gamelist}"
-				#Make sure the cheapest games are in the front
-				if 'PWYW' in gamenames:
-					gamelist += '{}.'.format("; ".join(gamenames['PWYW']))
-					gamenames.pop('PWYW')
-				for pricelevel in sorted(gamenames.keys()):
-					pricelevelText = str(pricelevel)
-					if isinstance(pricelevel, (int, float)):
-						pricelevelText = '${}'.format(pricelevel)
-					gamelist += ' {pricelevel}: {games}.'.format(pricelevel=pricelevelText, games="; ".join(gamenames[pricelevel]))
-			replytext = replytext.format(title=title, avgPrice=round(avgPrice, 2), totalMoney=round(totalMoney, 2), contributors=contributors, timeLeft=timeLeft, gamelist=gamelist)
+			#Totals aren't shown on the site immediately, but are edited into the page with Javascript. Get info from there
+			totalMoney = -1.0
+			contributors = -1
+			avgPrice = -1.0
+			timeLeft = u""
+			for scriptElement in page.find_all('script'):
+				script = scriptElement.text
+				if script.count("'initial_stats_data':") > 0:
+					#This script element contains the initial data
+					match = re.search("'initial_stats_data':(.+),", script)
+					if match is None:
+						print "Expected to find initial values, but failed!"
+						print script
+					else:
+						data = json.loads(match.group(1))
+						if 'rawtotal' in data:
+							totalMoney = data['rawtotal']
+						else:
+							print "Sales data found, but total amount is missing!"
+							print data
+						if 'numberofcontributions' in data and 'total' in data['numberofcontributions']:
+							contributors = int(data['numberofcontributions']['total'])
+						else:
+							print "Contributor data not found!"
+							print data
+
+						if totalMoney > -1.0 and contributors > -1:
+							avgPrice = totalMoney / contributors
+
+						timeLeftMatch = re.search('var timing = \{"end": (\d+)\};', script)
+						if timeLeftMatch:
+							timeLeft = SharedFunctions.durationSecondsToText(int(timeLeftMatch.group(1)) - time.time())
+
+						break
+
+			if totalMoney == -1.0 or contributors == -1 or avgPrice == -1.0:
+				replytext = u"Sorry, the data could not be retrieved. This is either because the site is down, or because of some weird bug. Please try again in a little while"
+			else:
+				replytext = u"{title} has an average price of ${avgPrice:.2f} and raised ${totalMoney:,} from {contributors:,} people."
+				gamelist = u""
+				if timeLeft != u"":
+					replytext += u" It will end in {timeLeft}."
+				#If we didn't find any games, pretend like nothing's wrong
+				if len(gamenames) > 0:
+					replytext += u" It contains {gamelist}"
+					#Make sure the cheapest games are in the front
+					if 'PWYW' in gamenames:
+						gamelist += u"{}.".format("; ".join(gamenames['PWYW']))
+						gamenames.pop('PWYW')
+					for pricelevel in sorted(gamenames.keys()):
+						pricelevelText = str(pricelevel)
+						if isinstance(pricelevel, (int, float)):
+							pricelevelText = u"${}".format(pricelevel)
+						gamelist += u" {pricelevel}: {games}.".format(pricelevel=pricelevelText, games=u"; ".join(gamenames[pricelevel]))
+				replytext = replytext.format(title=title, avgPrice=round(avgPrice, 2), totalMoney=round(totalMoney, 2), contributors=contributors, timeLeft=timeLeft, gamelist=gamelist)
 
 		message.bot.say(message.source, replytext)
