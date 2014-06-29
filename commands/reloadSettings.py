d7b831b25f5d007edb5502477fb6376787f17062
Didero <Didero@users.noreply.github.com>
2014-06-29 11:33:18 +0200

    [reloadSettings] Added way for bot to reload the api keys file too

--- commands/reloadSettings.py
+++ commands/reloadSettings.py
@@ -4,7 +4,7 @@ from IrcMessage import IrcMessage
 
 
 class Command(CommandTemplate):
-	triggers = ['reloadsettings']
+	triggers = ['reloadsettings', 'reloadkeys']
 	helptext = "Reloads the settings from disk. Only rarely useful"
 	adminOnly = True
 
@@ -13,23 +13,28 @@ class Command(CommandTemplate):
 		:type message: IrcMessage
 		"""
 		replytext = u""
-		#If the keyword 'all' was provided, reload the settings of all bots
-		if message.message.lower() == "all":
-			serversWithReloadFault = []
-			for serverfolder, botfactory in GlobalStore.bothandler.botfactories.iteritems():
-				success = botfactory.updateSettings()
-				if not success:
-					serversWithReloadFault.append(serverfolder)
+		if message.trigger == 'reloadsettings':
+			#If the keyword 'all' was provided, reload the settings of all bots
+			if message.message.lower() == "all":
+				serversWithReloadFault = []
+				for serverfolder, botfactory in GlobalStore.bothandler.botfactories.iteritems():
+					success = botfactory.updateSettings()
+					if not success:
+						serversWithReloadFault.append(serverfolder)
 
-			replytext = u"Reloaded all settings"
-			if len(serversWithReloadFault) > 0:
-				replytext += u" (error reloading settings for {})".format("; ".join(serversWithReloadFault))
-		#Otherwise, just reload the settings of this bot
-		else:
-			success = message.bot.factory.updateSettings()
-			if success:
-				replytext = u"Successfully reloaded settings for this bot"
+				replytext = u"Reloaded all settings"
+				if len(serversWithReloadFault) > 0:
+					replytext += u" (error reloading settings for {})".format("; ".join(serversWithReloadFault))
+			#Otherwise, just reload the settings of this bot
 			else:
-				replytext = u"An error occurred while trying to reload the settings for this bot, check the debug output for the cause"
+				success = message.bot.factory.updateSettings()
+				if success:
+					replytext = u"Successfully reloaded settings for this bot"
+				else:
+					replytext = u"An error occurred while trying to reload the settings for this bot, check the debug output for the cause"
+		elif message.trigger == 'reloadkeys':
+			#Reload the api keys
+			GlobalStore.commandhandler.loadApiKeys()
+			replytext = u"API keys file reloaded"
 				
 		message.bot.say(message.source, replytext)
