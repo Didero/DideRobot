21dc83ba39b1fa8f42cba0e1637e83ce79759e57
Didero <Didero@users.noreply.github.com>
2014-08-03 14:50:19 +0200

    [weather] Added forecast query, request timeout, and done minor clean-up

--- commands/weather.py
+++ commands/weather.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import json, time
+import datetime, json, time
 
 import requests
 
@@ -10,8 +10,8 @@ import GlobalStore
 
 
 class Command(CommandTemplate):
-	triggers = ['weather']
-	helptext = "Gets the weather for the provided city"
+	triggers = ['weather', 'forecast']
+	helptext = "Gets the weather or the forecast for the provided location"
 
 	def execute(self, message):
 		"""
@@ -25,15 +25,21 @@ class Command(CommandTemplate):
 			replytext = u"Please enter the name of a city"
 		else:
 			params = {"APPID": GlobalStore.commandhandler.apikeys.get("openweathermap", "key"), "q": message.message, "units": "metric"}
-			req = requests.get("http://api.openweathermap.org/data/2.5/weather", params=params)
+			requestType = 'weather'
+			if message.trigger == 'forecast':
+				requestType = 'forecast/daily'
+				params['cnt'] = 4  #Number of days to get forecast for
 			try:
+				req = requests.get("http://api.openweathermap.org/data/2.5/" + requestType, params=params, timeout=5.0)
 				data = json.loads(req.text)
+			except requests.exceptions.Timeout:
+				replytext = u"Sorry, the weather API took too long to respond. Please try again in a little while"
 			except ValueError:
 				replytext = u"Sorry, I couldn't retrieve that data. Try again in a little while, maybe it'll work then"
 				print "[weather] JSON load error. Data received:"
 				print req.text
 			else:
-				if data['cod'] != 200:
+				if data['cod'] != 200 and data['cod'] != "200":
 					if data['cod'] == 404 or data['cod'] == '404':
 						replytext = u"I'm sorry, I don't know where that is"
 					else:
@@ -42,37 +48,61 @@ class Command(CommandTemplate):
 						print data
 				else:
 					#We've got data! Parse it
-					#The highest wind angle where the direction applies
-					windDirectionTranslation = {11.25: 'N', 33.75: 'NNE', 56.25: 'NE', 78.75: 'ENE', 101.25: 'E', 123.75: 'ESE',
-												146.25: 'SE', 168.75: 'SSE', 191.25: 'S', 213.75: 'SSW', 236.25: 'SW',
-												258.75: 'WSW', 281.25: 'W', 303.75: 'WNW', 326.25: 'NW', 348.75: 'NNW', 360.0: 'N'}
-					windDirection = 'N'
-					for maxDegrees in sorted(windDirectionTranslation.keys()):
-						if data['wind']['deg'] < maxDegrees:
-							break
+					def getWindDirection(angle):
+						#The highest wind angle where the direction applies
+						windDirectionTranslation = {11.25: 'N', 33.75: 'NNE', 56.25: 'NE', 78.75: 'ENE', 101.25: 'E', 123.75: 'ESE',
+													146.25: 'SE', 168.75: 'SSE', 191.25: 'S', 213.75: 'SSW', 236.25: 'SW',
+													258.75: 'WSW', 281.25: 'W', 303.75: 'WNW', 326.25: 'NW', 348.75: 'NNW', 360.0: 'N'}
+						windDirection = 'N'
+						for maxDegrees in sorted(windDirectionTranslation.keys()):
+							if angle < maxDegrees:
+								break
+							else:
+								windDirection = windDirectionTranslation[maxDegrees]
+						return windDirection
+
+					def celsiusToFahrenheit(celsius):
+						return (celsius * 9 / 5) + 32
+
+
+					if message.trigger == 'weather':
+						dataAge = round((time.time() - data['dt']) / 60)
+						dataAgeDisplay = u""
+						if dataAge <= 0:
+							dataAge = u"brand new"
 						else:
-							windDirection = windDirectionTranslation[maxDegrees]
+							dataAgeDisplay = u"{dataAge:.0f} minute"
+							if dataAge > 1:
+								dataAgeDisplay += u"s"
+							dataAgeDisplay += u" old"
+							dataAgeDisplay = dataAgeDisplay.format(dataAge=dataAge)
 
-					tempInFahrenheit = (data['main']['temp'] * 9 / 5) + 32
+						#Not all replies include a placename
+						if 'name' in data and len(data['name']) > 0:
+							replytext += u"{city} ({country}): "
+						elif 'country' in data['sys'] and len(data['sys']['country']) > 0:
+							replytext += u"Somewhere in {country}: "
+						replytext += u"{tempC:.2g}°C / {tempF:.3g}°F, {weatherType}. Wind: {windSpeed} m/s, {windDir}. Humidity of {humidity}% (Data is {dataAge})"
+						replytext = replytext.format(city=data['name'], country=data['sys']['country'], tempC=data['main']['temp'], tempF=celsiusToFahrenheit(data['main']['temp']),
+													 weatherType=data['weather'][0]['description'], windSpeed=data['wind']['speed'], windDir=getWindDirection(data['wind']['deg']),
+													 humidity=data['main']['humidity'], dataAge=dataAgeDisplay)
 
-					dataAge = round((time.time() - data['dt']) / 60)
-					dataAgeDisplay = u""
-					if dataAge <= 0:
-						dataAge = u"brand new"
 					else:
-						dataAgeDisplay = u"{dataAge:.0f} minute"
-						if dataAge > 1:
-							dataAgeDisplay += u"s"
-						dataAgeDisplay += u" old"
-						dataAgeDisplay = dataAgeDisplay.format(dataAge=dataAge)
+						#Forecast
+						replytext = u"Forecast for "
+						if 'name' in data['city']:
+							replytext += u"{city} ({country}). "
+						else:
+							replytext += u"somewhere in {country}. "
+						replytext = replytext.format(city=data['city']['name'], country=data['city']['country'])
+						for day in data['list']:
+							dayname = datetime.datetime.utcfromtimestamp(day['dt']).strftime(u"%A")
 
-					#Not all replies include a placename
-					if 'name' in data and len(data['name']) > 0:
-						replytext += u"{city} ({country}): "
-					elif 'country' in data['sys'] and len(data['sys']['country']) > 0:
-						replytext += u"Somewhere in {country}: "
-					replytext += u"{tempC:.2g}°C / {tempF:.3g}°F, {weatherType}. Wind: {windSpeed} m/s, {windDir}. Humidity of {humidity}% (Data is {dataAge})"
-					replytext = replytext.format(city=data['name'], country=data['sys']['country'], tempC=data['main']['temp'], tempF=tempInFahrenheit, weatherType=data['weather'][0]['description'],
-												 windSpeed=data['wind']['speed'], windDir=windDirection, humidity=data['main']['humidity'], dataAge=dataAgeDisplay)
+							replytext += u"{dayname}: {minTempC:.2g}-{maxTempC:.2g}°C / {minTempF:.0f}-{maxTempF:.0f}°F, {weatherType}, {humidity}% hum., {windSpeed:.2g}m/s {windDir} wind.  "
+							replytext = replytext.format(dayname=dayname, minTempC=day['temp']['min'], maxTempC=day['temp']['max'],
+													minTempF=celsiusToFahrenheit(day['temp']['min']), maxTempF=celsiusToFahrenheit(day['temp']['max']),
+													humidity=day['humidity'], windSpeed=day['speed'], windDir=getWindDirection(day['deg']), weatherType=day['weather'][0]['description'])
+
 
 		message.bot.sendMessage(message.source, replytext)
+
