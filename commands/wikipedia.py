abf579dfdf6ada578921fff7c5425980e6847d4f
Didero <Didero@users.noreply.github.com>
2014-08-04 20:57:47 +0200

    [Wikipedia] Rigorous rewrite, now uses mobile version and BeautifulSoup, should lead to better results

--- commands/wikipedia.py
+++ commands/wikipedia.py
@@ -1,4 +1,4 @@
-import re
+import re, time
 
 import requests
 from bs4 import BeautifulSoup
@@ -11,63 +11,45 @@ class Command(CommandTemplate):
 	helptext = "Searches for the provided text on Wikipedia, and returns the start of the article, if it's found. {commandPrefix}wiki only returns the first sentence, {commandPrefix}wikipedia returns the first paragraph"
 
 	def execute(self, message):
-
+		"""
+		:type message: IrcMessage
+		"""
 		replytext = u""
 		replyLengthLimit = 300
 
 		if message.messagePartsLength == 0:
 			replytext = u"Please provide a term to search for"
 		else:
-			wikipediaPage = requests.get("http://en.wikipedia.org/wiki/Special:Search?search={}".format(message.message))
-			wikipediaText = wikipediaPage.content
-
-			#Loading the page into BeautifulSoup takes a full core a second or two, so it's not the best solution. So on to regex, yay!
-
-			#First remove all tables, so the output isn't from an intro or side data table
-			wikipediaText = re.sub('<table.*?>.+?</table>', '', wikipediaText, flags=re.DOTALL)
-
-			#The main article is put inside <p> tags
-			match = re.search(ur'<p.*?>(.+?)</p>', wikipediaText)
-			if not match:
-				replytext = u"No paragraph matches found!"
+			wikiPage = requests.get("http://en.m.wikipedia.org/w/index.php", params={'search': message.message})
+			wikitext = BeautifulSoup(wikiPage.content)
+			if len(wikiPage.history) == 0:
+				#We're still on the search page, so the search didn't lead to an article
+				replytext = u"Sorry, no article with that name found found. "
+				suggestions = wikitext.find(class_="searchdidyoumean")
+				if suggestions:
+					replytext += suggestions.text
 			else:
-				#Get the part between the <p> tags, the first paragraph of the article
-				replytext = match.group(1)
-				#Remove all the HTML tags
-				replytext = re.sub(ur'<.+?>', '', replytext)
-				#Remove the source references (the [1]'s)
-				replytext = re.sub(ur'\[\d+\]', '', replytext)
-				#Turn the thing back into unicode
-				replytext = replytext.decode('utf-8')
-				#print "[wiki] replytext before shortening: '{}'".format(replytext)
-				if message.trigger == 'wiki':
-					#Get just the first sentence
-					##print "First period at index: {}".format(replytext.find(u'. ')+1)
-					#Find the first sentence, which is the part until the first period. If it's a one-sentence paragraph, the sentence is the paragraph, otherwise there's a space and a new sentence
-					sentence = re.split('\.[ \Z]', replytext)[0].strip()
-					if len(sentence) > 0:
-						replytext = sentence
-						if not replytext.endswith('.'):
-							replytext += '.'
-					else:
-						print "[wiki] single sentence too short"
+				replytext = wikitext.find(id="content")  #The actual article is in a div with id 'content'
+				replytext = replytext.find('p')  #The article starts with a <p> tag
+				replytext = replytext.text  #Get the actual text, without any HTML
 
-				#Cap the length to prevent spamming, neatly at the last space so words don't get cut off
+				if message.trigger == 'wiki':
+					#Short reply
+					replytext = replytext.split(". ", 1)[0] + "."
+
+				#Shorten the reply if it's too long
 				if len(replytext) > replyLengthLimit:
 					replytext = replytext[:replyLengthLimit]
 
+					#Try not to chop up words
 					lastSpaceIndex = replytext.rfind(u' ')
 					if lastSpaceIndex > -1:
 						replytext = replytext[:lastSpaceIndex]
-					
-					while replytext.endswith(u',') or replytext.endswith(u'.'):
-						replytext = replytext[:-1]
+
 					replytext += u' [...]'
 
-				#If there's a link to a disambiguation page or something similar, it's in a div with a 'dablink' class
-				if 'dablink' in wikipediaText:
-					replytext += " (Multiple meanings)"
-
-				replytext = u"{replytext} ({url})".format(replytext=replytext, url=wikipediaPage.url)
+				#Add the URL to the end of the reply, so you can easily click to the full article
+				# (On the full Wikipedia, not the mobile version we're using)
+				replytext += u" ({})".format(wikiPage.url.replace('en.m', 'en', 1))
 
 		message.bot.say(message.source, replytext)
