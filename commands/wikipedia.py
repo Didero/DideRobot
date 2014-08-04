7ed09681c256649e77e6229931251ccbb04547b3
Didero <Didero@users.noreply.github.com>
2014-08-04 21:44:58 +0200

    [Wikipedia] Ignore tables at the start of articles; Added 'randomwiki' trigger, which returns the first sentence of a random article

--- commands/wikipedia.py
+++ commands/wikipedia.py
@@ -7,8 +7,10 @@ from CommandTemplate import CommandTemplate
 
 
 class Command(CommandTemplate):
-	triggers = ['wikipedia', 'wiki']
-	helptext = "Searches for the provided text on Wikipedia, and returns the start of the article, if it's found. {commandPrefix}wiki only returns the first sentence, {commandPrefix}wikipedia returns the first paragraph"
+	triggers = ['wikipedia', 'wiki', 'randomwiki']
+	helptext = "Searches for the provided text on Wikipedia, and returns the start of the article, if it's found. " \
+			   "{commandPrefix}wiki only returns the first sentence, {commandPrefix}wikipedia returns the first paragraph. " \
+			   "{commandPrefix}randomwiki returns a random wikipedia page"
 
 	def execute(self, message):
 		"""
@@ -17,23 +19,28 @@ class Command(CommandTemplate):
 		replytext = u""
 		replyLengthLimit = 300
 
-		if message.messagePartsLength == 0:
+		if message.messagePartsLength == 0 and message.trigger != 'randomwiki':
 			replytext = u"Please provide a term to search for"
 		else:
-			wikiPage = requests.get("http://en.m.wikipedia.org/w/index.php", params={'search': message.message})
+			wikiPage = None
+			if message.trigger == 'randomwiki':
+				wikiPage = requests.get('http://en.m.wikipedia.org/wiki/Special:Random/#/random')
+			else:
+				wikiPage = requests.get("http://en.m.wikipedia.org/w/index.php", params={'search': message.message})
 			wikitext = BeautifulSoup(wikiPage.content)
 			if len(wikiPage.history) == 0:
 				#We're still on the search page, so the search didn't lead to an article
 				replytext = u"Sorry, no article with that name found found. "
 				suggestions = wikitext.find(class_="searchdidyoumean")
 				if suggestions:
-					replytext += suggestions.text
+					replytext += suggestions.text + u"?"
 			else:
 				replytext = wikitext.find(id="content")  #The actual article is in a div with id 'content'
-				replytext = replytext.find('p')  #The article starts with a <p> tag
+				replytext = replytext.find('div')  #For some reason it's nested in another div tag
+				replytext = replytext.find('p', recursive=False)  #The article starts with a <p> tag in the root (ignore p-tags in tables)
 				replytext = replytext.text  #Get the actual text, without any HTML
 
-				if message.trigger == 'wiki':
+				if message.trigger != 'wikipedia':
 					#Short reply
 					replytext = replytext.split(". ", 1)[0] + "."
 
