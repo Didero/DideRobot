8eb0c7b5e787e9e4d9a0305f1884c98c9a63a505
Didero <Didero@users.noreply.github.com>
2014-10-17 09:53:40 +0200

    [urlTitleFinder] On Wikipedia URL, get the start of the article; instead of 'count()' use 'in', probably faster; add time-out to all requests; unify and minimize debug prints; general clean-up

--- commands/urlTitleFinder.py
+++ commands/urlTitleFinder.py
@@ -1,4 +1,4 @@
-import json, re, time
+import json, re
 import HTMLParser
 
 import requests
@@ -26,10 +26,10 @@ class Command(CommandTemplate):
 		"""
 		:type message: IrcMessage
 		"""
-		starttime = time.time()
+		timeout = 5.0  #In seconds
 		urlmatch = re.search(r"(https?://\S+)", message.message)
 		if not urlmatch:
-			print "(Title Retrieve module triggered, but no url found)"
+			print "[url] Module triggered, but no url found in message '{}'".format(message.rawText)
 		else:
 			url = urlmatch.group()
 			while url.endswith(")") or url.endswith('/'):
@@ -38,34 +38,23 @@ class Command(CommandTemplate):
 			try:
 				title = None
 				#There's some special cases for often used pages.
-				#Twitch!
-				if url.count('twitch.tv') > 0:
-					#Twitch
+				if 'twitch.tv' in url:
 					channelmatches = re.search("https?://w*\.twitch\.tv/([^/]+)", url)
 					if channelmatches:
 						channel = channelmatches.group(1)
 						channeldata = {}
 						isChannelOnline = False
 						twitchheaders = {'Accept': 'application/vnd.twitchtv.v2+json'}
-						twitchStreamPage = requests.get(u"https://api.twitch.tv/kraken/streams/" + channel, headers=twitchheaders)
+						twitchStreamPage = requests.get(u"https://api.twitch.tv/kraken/streams/" + channel, headers=twitchheaders, timeout=timeout)
 						streamdata = json.loads(twitchStreamPage.text.encode('utf-8'))
-						print "stream data:"
-						#for key, value in streamdata.iteritems():
-						#	print u"  {}: {}".format(key, value)
 						if 'stream' in streamdata and streamdata['stream'] is not None:
-						#	print "using Stream API call"
 							channeldata = streamdata['stream']['channel']
 							isChannelOnline = True
 						elif 'error' not in streamdata:
-						#	print "Using Channels API call"
-							twitchChannelPage = requests.get(u"https://api.twitch.tv/kraken/channels/" + channel, headers=twitchheaders)
+							twitchChannelPage = requests.get(u"https://api.twitch.tv/kraken/channels/" + channel, headers=twitchheaders, timeout=timeout)
 							channeldata = json.loads(twitchChannelPage.text.encode('utf-8'))
 
 						if len(channeldata) > 0:
-						#	print "Channel data:"
-						#	for key, value in channeldata.iteritems():
-						#		print u"  {}: {}".format(key, value)
-
 							title = u"Twitch - {username}"
 							if channeldata['game'] is not None:
 								title += u", playing {game}"
@@ -76,10 +65,9 @@ class Command(CommandTemplate):
 							else:
 								title += u" (Offline)"
 							title = title.format(username=channeldata['display_name'], game=channeldata['game'])
-				elif url.count('youtube.com') > 0 or url.count('youtu.be') > 0:
-					#Youtube!
+				elif 'youtube.com' in url or 'youtu.be' in url:
 					if not GlobalStore.commandhandler.apikeys.has_section('google') or not GlobalStore.commandhandler.apikeys.has_option('google', 'apikey'):
-						print "[ERROR] Google API key not found!"
+						print "[url] ERROR: Google API key not found!"
 					else:
 						#First we need to determine the video ID from something like this: http://www.youtube.com/watch?v=jmAKXADLcxY or http://youtu.be/jmAKXADLcxY
 						videoId = u""
@@ -94,12 +82,12 @@ class Command(CommandTemplate):
 							googleUrl = "https://www.googleapis.com/youtube/v3/videos"
 							params = {'part': 'statistics,snippet,contentDetails', 'id': videoId, 'key': GlobalStore.commandhandler.apikeys.get('google', 'apikey'),
 									  'fields': 'items/snippet(title,description),items/contentDetails/duration,items/statistics(viewCount,likeCount,dislikeCount)'}
-							googleJson = json.loads(requests.get(googleUrl, params=params).text.encode('utf-8'))
+							googleJson = json.loads(requests.get(googleUrl, params=params, timeout=timeout).text.encode('utf-8'))
 
 							if 'error' in googleJson:
-								print u"ERROR with Google requests. {}: {}. [{}]".format(googleJson['error']['code'], googleJson['error']['message'], json.dumps(googleJson).replace('\n',' '))
+								print u"[url] ERROR with Google requests. {}: {}. [{}]".format(googleJson['error']['code'], googleJson['error']['message'], json.dumps(googleJson).replace('\n',' '))
 							elif 'items' not in googleJson or len(googleJson['items']) != 1:
-								print u"Unexpected reply from Google API: {}".format(json.dumps(googleJson).replace('\n', ' '))
+								print u"[url] Unexpected reply from Google API: {}".format(json.dumps(googleJson).replace('\n', ' '))
 							else:
 								videoData = googleJson['items'][0]
 								durationtimes = SharedFunctions.parseIsoDate(videoData['contentDetails']['duration'])
@@ -119,16 +107,15 @@ class Command(CommandTemplate):
 								#likePercentage = int(videoData['statistics']['likeCount']) / int(videoData['statistics']['dislikeCount'])
 
 								title = u"{title} [{duration}, {viewcount:,} views]: {description}".format(title=videoData['snippet']['title'].strip(), duration=durationstring, viewcount=int(videoData['statistics']['viewCount']), description=description)
-				elif url.count('imgur.com') > 0:
+				elif 'imgur.com' in url:
 					if not GlobalStore.commandhandler.apikeys.has_section('imgur') or not GlobalStore.commandhandler.apikeys.has_option('imgur', 'clientid'):
-						print "[ERROR] Imgur API key not found!"
+						print "[url] ERROR: Imgur API key not found!"
 					else:
 						imageIdMatches = re.search('imgur\.com/([^.]+)', url, re.IGNORECASE)
 						if imageIdMatches is None:
-							print "No Imgur ID found in '{}'".format(url)
+							print "[url] No Imgur ID found in '{}'".format(url)
 						else:
 							imageId = imageIdMatches.group(1)
-							print "[imgur] initial id: '{}'".format(imageId)
 							isGallery = False
 							imageType = 'image'
 							if '/' in imageId:
@@ -136,17 +123,13 @@ class Command(CommandTemplate):
 									imageType = 'gallery/album'
 									isGallery = True
 								imageId = imageId[imageId.rfind('/')+1:]
-								print "Modified Imgur id: '{}'".format(imageId)
-							headers = {"Authorization": "Client-ID "+GlobalStore.commandhandler.apikeys.get('imgur','clientid')}
+							headers = {"Authorization": "Client-ID " + GlobalStore.commandhandler.apikeys.get('imgur','clientid')}
 							imgurUrl = "https://api.imgur.com/3/{type}/{id}".format(type=imageType, id=imageId)
-							print "url: {}".format(url)
-							imgurDataPage = requests.get(imgurUrl, headers=headers)
-							print "page: {}".format(imgurDataPage.text.encode('utf-8'))
+							imgurDataPage = requests.get(imgurUrl, headers=headers, timeout=timeout)
 							imgdata = json.loads(imgurDataPage.text.encode('utf-8'))
-							if imgdata['success'] != True or imgdata['status'] != 200:
+							if imgdata['success'] is not True or imgdata['status'] != 200:
 								print "[UrlTitle Imgur] Error while retrieving image data: {}".format(imgurDataPage.text.encode('utf-8'))
 							else:
-								#print imgurData
 								imgdata = imgdata['data']
 								if imgdata['title'] is None:
 									imgdata['title'] = u"No Title"
@@ -167,24 +150,21 @@ class Command(CommandTemplate):
 								if 'nsfw' in imgdata and imgdata['nsfw'] == True:
 									title += u" (NSFW!)"
 								title = title.format(imgdata=imgdata)
-				elif url.count('twitter.com') > 0:
+				elif 'twitter.com' in url:
 					tweetMatches = re.search('twitter.com/(?P<name>[^/]+)(?:/status/(?P<id>[^/]+).*)?', url)
 					if not tweetMatches:
-						print "No twitter matches found in '{}'".format(url)
+						print "[url] No twitter matches found in '{}'".format(url)
 					else:
-						print "Tweetmatches: {}".format(tweetMatches.groupdict())
-						#print "[UrlTitleFinder Twitter] Username is '{}', tweet id is '{}'".format(tweetMatches.group('name'), tweetMatches.group('id'))						
 						if not GlobalStore.commandhandler.apikeys.has_section('twitter') or not GlobalStore.commandhandler.apikeys.has_option('twitter', 'tokentype') or not GlobalStore.commandhandler.apikeys.has_option('twitter', 'token'):
-							print "[UrlTitleFinder] ERROR: Twitter API token info not found!"
+							print "[url] ERROR: Twitter API token info not found!"
 						else:
 							headers = {"Authorization": "{} {}".format(GlobalStore.commandhandler.apikeys.get('twitter', 'tokentype'),
 												 GlobalStore.commandhandler.apikeys.get('twitter', 'token'))}
 							if 'id' in tweetMatches.groupdict() and tweetMatches.group('id') is not None:
 								#Specific tweet
 								twitterUrl = "https://api.twitter.com/1.1/statuses/show.json?id={id}".format(id=tweetMatches.group('id'))
-								twitterDataPage = requests.get(twitterUrl, headers=headers)
+								twitterDataPage = requests.get(twitterUrl, headers=headers, timeout=timeout)
 								twitterdata = json.loads(twitterDataPage.text.encode('utf-8'))
-								print twitterdata
 
 								title = u"@{username} ({name}): {text} [{timestamp}]".format(username=twitterdata['user']['screen_name'], name=twitterdata['user']['name'],
 																		text=twitterdata['text'], timestamp=twitterdata['created_at'])
@@ -198,23 +178,23 @@ class Command(CommandTemplate):
 								if 'verified' in twitterdata and twitterdata['verified'] == True:
 									title += u". Verified account"
 								title = title.format(**twitterdata)
+				elif re.match('https?://.{2}(?:\.m)?\.wikipedia.org', url, re.IGNORECASE):
+					title = GlobalStore.commandhandler.runCommandFunction('getWikipediaArticle', None, url, False)
 
 				#If nothing has been found so far, just display whatever is between the <title> tags
 				if title is None:
-					print "Using default title search"
 					#Check here and not later because sites like Imgur can have .jpg URLs and we still want to check those
 					extensionsToIgnore = ['.jpg', '.jpeg', '.gif', '.png', '.bmp', '.avi', '.wav', '.mp3', '.zip', '.rar', '.7z', '.pdf']
 					for ext in extensionsToIgnore:
 						if url.endswith(ext):
-							print "Skipping title search, ignorable extension"
 							return
-					titlematch = re.search(r'<title.*?>(.+)</title>', requests.get(url).text, re.DOTALL)
-					if not titlematch:
-						print "No title found on page '{}'".format(url)
-					else:
+					titlematch = re.search(r'<title.*?>(.+)</title>', requests.get(url, timeout=timeout).text, re.DOTALL)
+					if titlematch:
 						title = titlematch.group(1).replace('\n', '').strip()
+			except requests.exceptions.Timeout:
+				print "[url] '{}' took too long to respond, ignoring".format(url)
 			except requests.exceptions.ConnectionError as error:
-				print "(A connection error occurred while trying to retrieve '{}': {})".format(url, error)
+				print "[url] A connection error occurred while trying to retrieve '{}': {}".format(url, error)
 
 			#Finally, display the result of all the hard work, if there was any
 			if title is not None:
@@ -225,5 +205,4 @@ class Command(CommandTemplate):
 				if len(title) > 250:
 					title = title[:250] + "..."
 
-				print "[urlTitleFinder] Time taken: {} seconds".format(time.time() - starttime)
 				message.bot.say(message.source, u"Title: {}".format(title))
