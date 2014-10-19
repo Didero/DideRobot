bc43653a5c733b1d711306a3e99375592b817fde
Didero <Didero@users.noreply.github.com>
2014-10-19 13:33:07 +0200

    [Wolfram] Made the Wolfram module callable by other modules through the CommandFunctions system

--- commands/wolframalpha.py
+++ commands/wolframalpha.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ElementTree
 
-#from bs4 import BeautifulSoup
 import requests
 
 from CommandTemplate import CommandTemplate
@@ -11,71 +10,106 @@ class Command(CommandTemplate):
 	triggers = ['wolfram', 'wolframalpha', 'wa']
 	helptext = "Sends the provided query to Wolfram Alpha and shows the results, if any"
 	callInThread = True  #WolframAlpha can be a bit slow
-	
+
+	def onLoad(self):
+		GlobalStore.commandhandler.addCommandFunctions(__file__, "fetchWolframAlphaData", self.fetchWolframData, "searchWolframAlpha", self.searchWolfram)
+
 	def execute(self, message):
+		"""
+		:type message: IrcMessage
+		"""
 		replystring = u""
-		if not GlobalStore.commandhandler.apikeys.has_section('wolframalpha') or not GlobalStore.commandhandler.apikeys.has_option('wolframalpha', 'key'):
-			replystring = u"No API key for Wolfram Alpha found. That's kinda sloppy, owner"
-		elif message.messagePartsLength == 0:
+		if message.messagePartsLength == 0:
 			replystring = u"No query provided. I'm not just gonna make stuff up to send to Wolfram Alpha, I've got an API call limit! Add your query after the command."
 		else:
-			searchstring = message.message
-			params = {'appid': GlobalStore.commandhandler.apikeys.get('wolframalpha', 'key'), 'podindex': '1,2,3', 'input': searchstring}
-			apireturn = requests.get("http://api.wolframalpha.com/v2/query", params=params)
-			xmltext = apireturn.text
-			xmltext = xmltext.replace(r'\:', r'\u')  #weird WolframAlpha way of writing Unicode
-			#Replace '\u0440' and the like with the actual character (first encode with latin-1 and not utf-8, otherwise pound signs and stuff mess up with a weird accented A in front)
-			try:
-				xmltext = unicode(xmltext.encode('latin-1'), encoding='unicode-escape')
-			except Exception as e:
-				print "[Wolfram] Error while turning unicode escapes into words with latin-1, using utf-8 ({})".format(str(e))
-				xmltext = unicode(xmltext.encode('utf-8'), encoding='unicode-escape')
-			xml = ElementTree.fromstring(xmltext.encode('utf8'))
-			if xml.attrib['error'] != 'false':
-				replystring = u"An error occurred"
-				print "[Wolfram] An error occurred for the search query '{}'. Reply:".format(message.message)
-				print xmltext.encode('utf-8')
-			elif xml.attrib['success'] != 'true':
-				replystring = u"No results found, sorry"
-				#Most likely no results were found. See if there are suggestions for search improvements
-				if xml.find('didyoumeans') is not None:
-					didyoumeans = xml.find('didyoumeans').findall('didyoumean')
-					suggestions = []
-					for didyoumean in didyoumeans:
-						if didyoumean.attrib['level'] != 'low':
-							suggestion = didyoumean.text.replace('\n','').strip()
-							if len(suggestion) > 0:
-								suggestions.append(suggestion)
-					if len(suggestions) > 0:
-						replystring += u". Did you perhaps mean: {}".format(", ".join(suggestions))
-			else:
-				pods = xml.findall('pod')
-				resultFound = False
-				for pod in pods[1:]:
-					if pod.attrib['title'] == "Input":
+			replystring = self.searchWolfram(message.message)
+		message.bot.sendMessage(message.source, replystring)
+
+	def fetchWolframData(self, query, podsToFetch=3):
+		#First check if there is an API key
+		if not GlobalStore.commandhandler.apikeys.has_section('wolframalpha') or not GlobalStore.commandhandler.apikeys.has_option('wolframalpha', 'key'):
+			return (False, u"Error: No Wolfram Alpha API key found")
+
+		replystring = u""
+		params = {'appid': GlobalStore.commandhandler.apikeys.get('wolframalpha', 'key'), 'input': query}
+		if podsToFetch > 0:
+			podIndexParam = ""
+			for i in xrange(1, podsToFetch):
+				podIndexParam += "{},".format(i)
+			podIndexParam = podIndexParam[:-1]
+			params['podindex'] = podIndexParam
+		apireturn = None
+		try:
+			apireturn = requests.get("http://api.wolframalpha.com/v2/query", params=params, timeout=15.0)
+		except requests.exceptions.Timeout:
+			return (False, u"Sorry, Wolfram Alpha took too long to respond")
+		xmltext = apireturn.text
+		xmltext = xmltext.replace(r'\:', r'\u')  #weird WolframAlpha way of writing Unicode
+		#Replace '\u0440' and the like with the actual character (first encode with latin-1 and not utf-8, otherwise pound signs and stuff mess up with a weird accented A in front)
+		try:
+			xmltext = unicode(xmltext.encode('latin-1'), encoding='unicode-escape')
+		except Exception as e:
+			print "[Wolfram] Error while turning unicode escapes into words with latin-1, using utf-8 ({})".format(str(e))
+			xmltext = unicode(xmltext.encode('utf-8'), encoding='unicode-escape')
+		return (True, xmltext)
+
+	
+	def searchWolfram(self, query, podsToParse=3, cleanUpText=True, includeUrl=True):
+		replystring = u""
+		wolframResult = self.fetchWolframData(query, podsToParse)
+		#First check if the query succeeded
+		if not wolframResult[0]:
+			return wolframResult[1]
+
+		xml = ElementTree.fromstring(wolframResult[1].encode('utf8'))
+		if xml.attrib['error'] != 'false':
+			replystring = u"Sorry, an error occurred. Tell my owner(s) to check the error log"
+			print "[Wolfram] An error occurred for the search query '{}'. Reply:".format(query)
+			print wolframResult[1].encode('utf-8')
+		elif xml.attrib['success'] != 'true':
+			replystring = u"No results found, sorry"
+			#Most likely no results were found. See if there are suggestions for search improvements
+			if xml.find('didyoumeans') is not None:
+				didyoumeans = xml.find('didyoumeans').findall('didyoumean')
+				suggestions = []
+				for didyoumean in didyoumeans:
+					if didyoumean.attrib['level'] != 'low':
+						suggestion = didyoumean.text.replace('\n','').strip()
+						if len(suggestion) > 0:
+							suggestions.append(suggestion)
+				if len(suggestions) > 0:
+					replystring += u". Did you perhaps mean: {}".format(", ".join(suggestions))
+		else:
+			pods = xml.findall('pod')
+			resultFound = False
+			for pod in pods[1:]:
+				if pod.attrib['title'] == "Input":
+					continue
+				for subpod in pod.findall('subpod'):
+					text = subpod.find('plaintext').text
+					if text is None:
 						continue
-					for subpod in pod.findall('subpod'):
-						text = subpod.find('plaintext').text
-						if text is None:
-							continue
+					if cleanUpText:
 						text = text.replace('\n', ' ').strip()
-						#If there's no text in this pod (for instance if it's just an image)
-						if len(text) == 0:
-							continue
-						#If the result is useless (searching for '3 usd' for instance returns coin weight first, starts with an opening bracket), skip it
-						elif text.startswith('('):
-							continue
-						replystring += text
-						resultFound = True
-						break
-					if resultFound:
-						break
+					#If there's no text in this pod (for instance if it's just an image)
+					if len(text) == 0:
+						continue
+					#If the result is useless (searching for '3 usd' for instance returns coin weight first, starts with an opening bracket), skip it
+					elif text.startswith('('):
+						continue
+					replystring += text
+					resultFound = True
+					break
+				if resultFound:
+					break
 
-				if not resultFound:
-					replystring += u"Sorry, results were either images or non-existent"
+			if not resultFound:
+				replystring += u"Sorry, results were either images or non-existent"
 
+		if cleanUpText:
 			replystring = replystring.replace('  ', ' ')
-			#Add the search url
-			replystring += u" (http://www.wolframalpha.com/input/?i={})".format(searchstring.replace(" ", "+"))
+		#Add the search url
+		if includeUrl:
+			replystring += u" (http://www.wolframalpha.com/input/?i={})".format(query.replace(" ", "+"))
 			
-		message.bot.say(message.source, replystring)
+		return replystring
