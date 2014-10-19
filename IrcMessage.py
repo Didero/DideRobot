f013b9fb9b7a454d9721a823e08e24a4a958bb99
Didero <Didero@users.noreply.github.com>
2014-10-19 13:34:46 +0200

    Made triggers from messages lowercase, so commands aren't case-sensitive anymore

--- IrcMessage.py
+++ IrcMessage.py
@@ -44,11 +44,11 @@ class IrcMessage(object):
 			#Collect information about the possible command in this message
 			if self.rawText.startswith(bot.factory.commandPrefix):
 				#Get the part from the end of the command prefix to the first space (the 'help' part of '!help say')
-				self.trigger = self.rawText[bot.factory.commandPrefixLength:].split(" ", 1)[0].strip()
+				self.trigger = self.rawText[bot.factory.commandPrefixLength:].split(" ", 1)[0].strip().lower()
 				self.message = self.rawText[bot.factory.commandPrefixLength + len(self.trigger):].strip()
 			#Check if the text doesn't start with the nick of the bot, 'DideRobot: help'
 			elif self.rawText.startswith(bot.nickname + ": ") and len(self.rawText) > len(bot.nickname) + 2:
-				self.trigger = self.rawText.split(u" ", 2)[1].strip()
+				self.trigger = self.rawText.split(" ", 2)[1].strip().lower()
 				self.message = self.rawText[len(bot.nickname) + len(self.trigger) + 3:].strip()
 			else:
 				self.trigger = None
