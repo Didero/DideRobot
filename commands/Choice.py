fbd15a47131cb860e86a7aeace47ec1a232dbf2b
Didero <Didero@users.noreply.github.com>
2015-02-10 10:22:24 +0100

    [Choice] Vastly decrease the chance of a repeated reply sentence

--- commands/Choice.py
+++ commands/Choice.py
@@ -7,6 +7,19 @@ class Command(CommandTemplate):
 	triggers = ['choice', 'choose']
 	helptext = "Helps you make a choice between options in a comma-separated list"
 
+	possibleReplies = ["{}", "Hmm, I'd go with {}", "Out of those, {} sounds the least bad", "{}, obviously",
+					   "Let's go with... {}. No wait! No, yeah, that one", "I don't know! *rolls dice* Seems you should go for {}",
+					   "Pick {0}, pick {0}!", "Eh, {} will do", "Why not {}?", "The first one! The last one! {}!", "Just pick {}"]
+
+	def pickRandomReply(self):
+		#Based on a suggestion by ekimekim
+		while True:
+			#Shuffle the list initially
+			random.shuffle(self.possibleReplies)
+			#Then just feed a reply every time one is requested. Once we run out, the list is reshuffled, ready to start again
+			for reply in self.possibleReplies:
+				yield reply
+
 	def execute(self, message):
 		"""
 		:type message: IrcMessage.IrcMessage
@@ -23,9 +36,6 @@ class Command(CommandTemplate):
 			if len(choices) == 1:
 				replytext = "Ooh, that's a tough one. I'd go with the first option, seeing as there is only one"
 			else:
-				possibleReplies = ["{}", "Hmm, I'd go with {}", "Out of those, {} sounds the least bad", "{}, obviously",
-								   "Let's go with... {}. No wait! No, yeah, that one", "I don't know! *rolls dice* Seems you should go for {}",
-								   "Pick {0}, pick {0}!", "Eh, {} will do", "Why not {}?", "The first one! The last one! {}!"]
 				#Pick a random reply sentence, and then add in a random choice from the provided list, enclosed in quotes
-				replytext = random.choice(possibleReplies).format('"' + random.choice(choices).strip() + '"')
+				replytext = self.pickRandomReply().format('"' + random.choice(choices).strip() + '"')
 		message.bot.sendMessage(message.source, replytext)
