4c2da50e9bf12f4ad9fe2905cebdf2356563d225
Didero <Didero@users.noreply.github.com>
2015-10-23 19:44:12 +0200

    [Help] Only show admin commands when an admin calls the module; Minor improvements readability and neatness improvements

--- commands/help.py
+++ commands/help.py
@@ -15,21 +15,24 @@ class Command(CommandTemplate):
 		#First get all the existing triggers, since we either need to list them or check if the provided one exists
 		triggerlist = {}
 		shortTriggerlist = {}
+		isUserAdmin = message.bot.factory.isUserAdmin(message.user, message.userNickname, message.userAddress)
 		for commandname, command in GlobalStore.commandhandler.commands.iteritems():
-			if command.showInCommandList and GlobalStore.commandhandler.isCommandAllowedForBot(message.bot, commandname) and len(command.triggers) > 0:
+			if command.showInCommandList and (isUserAdmin or not command.adminOnly) and len(command.triggers) > 0 and GlobalStore.commandhandler.isCommandAllowedForBot(message.bot, commandname):
 				shortTriggerlist[command.triggers[0]] = command
 				for trigger in command.triggers:
 					triggerlist[trigger] = command
 				
 		replytext = u""
 		#Check if a command has been passed as argument
-		command = message.message.lower()
-		#Remove the command prefix if it's there, because the lookup doesn't have it
-		if command.startswith(message.bot.factory.commandPrefix):
-				command = command[message.bot.factory.commandPrefixLength:]
+		command = None
+		if message.messagePartsLength > 0:
+			command = message.message.lower()
+			#Remove the command prefix if it's there, because the lookup doesn't have it
+			if command.startswith(message.bot.factory.commandPrefix):
+					command = command[message.bot.factory.commandPrefixLength:]
 		
 		#check if the provided argument exists
-		if command in triggerlist:
+		if command and command in triggerlist:
 			#'!help, !helpfull: '
 			replytext = "{commandPrefix}" + ", {commandPrefix}".join(triggerlist[command].triggers)
 			#Since some modules have '{commandPrefix}' in their helptext, turn that into the actual command prefix
@@ -42,14 +45,13 @@ class Command(CommandTemplate):
 		# show a list of available commands
 		else:
 			#If a command was provided but not found, apologize even though it's not our fault
-			if command != "":
+			if command:
 				replytext = "I don't know that command, sorry. "
 			commandslist = ""
 			if message.trigger == 'helpfull':
 				commandslist = ", ".join(sorted(triggerlist.keys()))
 			else:
 				commandslist = ", ".join(sorted(shortTriggerlist.keys()))
-			replytext += "Commands loaded: {commandslist}. Type '{prefix}help [commandname]' for info on how to use that command"\
-				.format(commandslist=commandslist, prefix=message.bot.factory.commandPrefix)
+			replytext += "Commands loaded: {}. Type '{}help [commandname]' for info on how to use that command".format(commandslist, message.bot.factory.commandPrefix)
 		
 		message.bot.sendMessage(message.source, replytext, 'say')
