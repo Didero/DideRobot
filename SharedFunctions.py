035c29c2606e6265234f29d35a465788b6928551
Didero <Didero@users.noreply.github.com>
2015-12-18 19:57:11 +0100

    Add comment to bolding function

--- SharedFunctions.py
+++ SharedFunctions.py
@@ -225,6 +225,7 @@ def addSeparatorsToString(listOfStrings, separator='|'):
 	return formattedSeparator.join(listOfStrings)
 
 def makeTextBold(s):
-	return '\x02' + s + '\x0f'
+	return '\x02' + s + '\x0f'  #\x02 is the 'bold' control character, '\x0f' cancels all decorations
+
 
 
