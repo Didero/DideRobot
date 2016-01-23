d74cc63164db9a99299bef67069aafaea0df1108
Didero <Didero@users.noreply.github.com>
2016-01-23 13:10:45 +0100

    Add function to shorten URLs through goo.gl

--- SharedFunctions.py
+++ SharedFunctions.py
@@ -213,5 +213,24 @@ def getGreySeparator():
 def makeTextBold(s):
 	return '\x02' + s + '\x0f'  #\x02 is the 'bold' control character, '\x0f' cancels all decorations
 
+def shortenUrl(longUrl):
+	if 'google' not in GlobalStore.commandhandler.apikeys:
+		logging.getLogger('DideRobot').error("Url shortening requested but Google API key not found")
+		return (False, longUrl, "No Google API key found")
+	#The Google shortening API requires the url key in the POST message body for some reason, hence 'json' and not 'data'
+	response = requests.post('https://www.googleapis.com/urlshortener/v1/url?key=' + GlobalStore.commandhandler.apikeys['google'],
+				  json={'longUrl': longUrl})
+	try:
+		data = response.json()
+	except ValueError:
+		return (False, longUrl, "Reply was not JSON", response.text)
+	else:
+		if 'error' in data:
+			return (False, longUrl, "An error occurred: {}", data)
+		#If we reach here, everything is fine.
+		# 'id' contains the shortened URL
+		# 'longUrl' usually contains the original URL, but sometimes it is also the result of redirects or canonization
+		return (True, data['id'], data['longUrl'])
+
 
 
