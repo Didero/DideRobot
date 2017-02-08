12f8e79f735bb0505ca1fc6d9f48cb44fbf4af0a
Didero <Didero@users.noreply.github.com>
2017-02-08 12:19:47 +0100

    [Join] Improve channel prefix removal, and already-joined check

--- commands/join.py
+++ commands/join.py
@@ -16,15 +16,15 @@ class Command(CommandTemplate):
 			replytext = "Please provide a channel for me to join"
 		else:
 			channel = message.messageParts[0].lower()
-			if channel.startswith('#'):
-				channel = channel.lstrip('#')
+			#Strip all possible channel prefixes
+			channelWithoutPrefix = channel.lstrip("".join(message.bot.supported.getFeature("CHANTYPES")))
 
-			if '#' + channel in message.bot.channelsUserList:
+			if channel in message.bot.channelsUserList or '#' + channelWithoutPrefix in message.bot.channelsUserList:
 				replytext = "I'm already there, waiting for you. You're welcome!"
-			elif channel not in message.bot.factory.settings['allowedChannels'] and not message.bot.factory.isUserAdmin(message.user, message.userNickname, message.userAddress):
+			elif channelWithoutPrefix not in message.bot.factory.settings['allowedChannels'] and not message.bot.factory.isUserAdmin(message.user, message.userNickname, message.userAddress):
 				replytext = "I'm sorry, I'm not allowed to go there. Please ask my admin(s) for permission"
 			else:
-				replytext = "All right, I'll go to #{}. See you there!".format(channel)
+				replytext = "All right, I'll go to '{}'. See you there!".format(channel)
 				message.bot.join(channel)
 
 		message.reply(replytext, "say")
