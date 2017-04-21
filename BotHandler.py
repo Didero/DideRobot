445bdd9fd90e5b5b8d4be49faf62ff015978209a
Didero <Didero@users.noreply.github.com>
2017-04-21 11:08:37 +0200

    Simplify shutdown procedure, by making 'shutdown' call 'stopBotFactory' instead of copying it

--- BotHandler.py
+++ BotHandler.py
@@ -46,27 +46,22 @@ class BotHandler:
 			return True
 
 	def shutdown(self, quitmessage='Shutting down...'):
-		#Give all modules a chance to close properly
-		GlobalStore.commandhandler.unloadAllCommands()
 		#Give all bots the same quit message
 		quitmessage = quitmessage.encode('utf-8')
-		for serverfolder, botfactory in self.botfactories.iteritems():
-			if botfactory.bot:
-				botfactory.shouldReconnect = False
-				botfactory.bot.quit(quitmessage)
-		self.botfactories = {}
-		#Give all bots a little time to shut down
-		GlobalStore.reactor.callLater(4.0, GlobalStore.reactor.stop)
+		for serverfolder in self.botfactories.keys():
+			self.stopBotfactory(serverfolder, quitmessage, False)
+
 
 	def unregisterFactory(self, serverfolder, isRestarting=False):
 		if serverfolder in self.botfactories:
 			del self.botfactories[serverfolder]
+			self.logger.info("Successfully unregistered bot '{}'".format(serverfolder))
 			#If there's no more bots running, there's no need to hang about
 			if len(self.botfactories) == 0:
 				if isRestarting:
 					self.logger.info("Last bot unregistered, not shutting down because restart expected")
 				else:
 					self.logger.info("Out of bots, shutting down!")
-					GlobalStore.reactor.callLater(2.0, GlobalStore.reactor.stop)
-			else:
-				self.logger.info("Successfully unregistered bot '{}', {} bots left: {}".format(serverfolder, len(self.botfactories), "; ".join(self.botfactories.keys())))
+					# Give all modules and bots a chance to close properly
+					GlobalStore.commandhandler.unloadAllCommands()
+					GlobalStore.reactor.callLater(3.0, GlobalStore.reactor.stop)
