53d28f0e73e43229feb81381edd0dcd0d0520829
Didero <Didero@users.noreply.github.com>
2017-09-03 13:38:38 +0200

    Prevent access to files outside DideRobot's folder
    (Thanks for pointing this out, ekimekim)

--- SharedFunctions.py
+++ SharedFunctions.py
@@ -101,6 +101,15 @@ def downloadTweet(username, tweetId):
 	#Otherwise, make the single-item tweet list just the tweet
 	return (True, downloadedTweet[1][0])
 
+
+def isAllowedPath(path):
+	#This function checks whether the provided path is inside the bot's data folder
+	# To prevent people adding "../.." to some bot calls to have free access to the server's filesystem
+	if not os.path.abspath(path).startswith(GlobalStore.scriptfolder):
+		logger.warning("[SharedFunctions] Somebody is trying to leave the bot's file systems by calling filename '{}'".format(path))
+		return False
+	return True
+
 def getLineCount(filename):
 	#Set a default in case the file has no lines
 	linecount = -1  #'-1' so with the +1 at the end it ends up a 0 for an empty file
@@ -115,6 +124,12 @@ def getLineFromFile(filename, wantedLineNumber):
 	"""Returns the specified line number from the provided file (line number starts at 0)"""
 	if not filename.startswith(GlobalStore.scriptfolder):
 		filename = os.path.join(GlobalStore.scriptfolder, filename)
+	#Check if it's an allowed path
+	if not isAllowedPath(filename):
+		return None
+	if not os.path.exists(filename):
+		logger.error(u"Can't read line {} from file '{}'; file does not exist".format(wantedLineNumber, filename))
+		return None
 	with codecs.open(filename, 'r', 'utf-8') as f:
 		for lineNumber, line in enumerate(f):
 			if lineNumber == wantedLineNumber:
@@ -129,12 +144,14 @@ def getRandomLineFromFile(filename, linecount=None):
 	return getLineFromFile(filename, random.randrange(0, linecount))
 
 def getAllLinesFromFile(filename):
-	if not os.path.exists(filename):
-		logger.error(u"Can't read lines from file '{}'; it does not exist".format(filename))
-		return None
 	#Make sure it's an absolute filename
 	if not filename.startswith(GlobalStore.scriptfolder):
 		filename = os.path.join(GlobalStore.scriptfolder, filename)
+	if not isAllowedPath(filename):
+		return None
+	if not os.path.exists(filename):
+		logger.error(u"Can't read lines from file '{}'; it does not exist".format(filename))
+		return None
 	#Get all the lines!
 	with codecs.open(filename, 'r', 'utf-8') as linesfile:
 		return linesfile.readlines()
