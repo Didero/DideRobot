26584551207de5e9af0ef077ae233b52f42e00de
Didero <Didero@users.noreply.github.com>
2018-02-03 13:52:48 +0100

    Add 'MAX_MESSAGE_LENGTH' constant for commands
    This is the maximum number of characters commands should put in a message

--- Constants.py
+++ Constants.py
@@ -1,5 +1,6 @@
 CTCP_DELIMITER = chr(1)
 MAX_LINE_LENGTH = 450  #Officially 512 bytes including the newline characters, but let's be on the safe side
+MAX_MESSAGE_LENGTH = 300  #The maximum number of characters in message to send. Less than max line length because that includes channel and nickname
 CHANNEL_PREFIXES = "#&!+.~"  #All the characters that could possibly indicate something is a channel name (usually just '#' though)
 #Since a grey separator is often used to separate parts of a message, provide an easy way to get one
 GREY_SEPARATOR = u' \x0314|\x0f '  #'\x03' is the 'color' control char, 14 is grey, and '\x0f' is the 'reset' character ending any decoration
