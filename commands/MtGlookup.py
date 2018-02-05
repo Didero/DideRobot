8845365cd2160b364fb076f47cf9a9e8a863fd63
Didero <Didero@users.noreply.github.com>
2018-02-05 18:41:39 +0100

    [MTG] Remove 'token' field in booster packs

--- commands/MtGlookup.py
+++ commands/MtGlookup.py
@@ -23,7 +23,7 @@ class Command(CommandTemplate):
 	callInThread = True  #If a call causes a card update, make sure that doesn't block the whole bot
 
 	areCardfilesInUse = False
-	dataFormatVersion = '4.3'
+	dataFormatVersion = '4.3.1'
 
 	def onLoad(self):
 		GlobalStore.commandhandler.addCommandFunction(__file__, 'searchMagicTheGatheringCards', self.searchCards)
@@ -710,7 +710,7 @@ class Command(CommandTemplate):
 		definitions = []
 		#Lists of what to do with certain set keys
 		setKeysToRemove = ('border', 'magicRaritiesCodes', 'mkm_id', 'mkm_name', 'oldCode', 'onlineOnly', 'translations')
-		raritiesToRemove = ('checklist', 'double faced', 'draft-matters', 'foil', 'marketing', 'power nine', 'timeshifted purple')
+		raritiesToRemove = ('checklist', 'double faced', 'draft-matters', 'foil', 'marketing', 'power nine', 'timeshifted purple', 'token')
 		raritiesToRename = {'land': 'basic land', 'urza land': 'land — urza’s'}  #Non-standard rarities are interpreted as regexes for type
 		rarityPrefixesToRemove = {'foil ': 5, 'timeshifted ': 12}  #The numbers are the string length, saves a lot of 'len()' calls
 		#Lists of what to do with certain card keys
