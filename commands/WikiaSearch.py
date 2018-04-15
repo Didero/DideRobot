778832c36aadfbe0d37652a0310b8b5371ccaed3
Didero <Didero@users.noreply.github.com>
2018-04-15 12:11:46 +0200

    [Wikia] Improve error checking, and rewrite the 'wikiarandom' path to make use of that

--- commands/WikiaSearch.py
+++ commands/WikiaSearch.py
@@ -1,8 +1,19 @@
+import json
+
 import requests
 
 from CommandTemplate import CommandTemplate
 from IrcMessage import IrcMessage
 import Constants
+
+
+class WikiaApiErrors(object):
+	TIMEOUT = 0
+	INVALID_WIKIA_NAME = 1
+	INVALID_API_CALL = 2
+	API_DISABLED = 3
+	NO_RESULTS_FOUND = 4
+	GENERIC_ERROR = 100
 
 
 class Command(CommandTemplate):
@@ -19,21 +30,27 @@ class Command(CommandTemplate):
 		if message.messagePartsLength == 0:
 			return message.reply("Please tell me which Wikia wiki you want me to search, there's a BILLION of 'em", "say")
 
-		#Getting a random page only needs a wiki name
-		if message.trigger == 'wikiarandom':
-			return message.reply(Command.getRandomWikiaArticle(message.messageParts[0])[1])
-
 		#Searches need a search term
-		if message.messagePartsLength == 1:
+		if message.trigger != 'wikiarandom' and message.messagePartsLength == 1:
 			return message.reply("What do you want me to search for on the {} Wikia wiki?".format(message.messageParts[0]), "say")
 
-		searchterm = " ".join(message.messageParts[1:])
-		success, articleTitleOrError = self.searchForArticleTitle(message.messageParts[0], searchterm)
+		wikiName = message.messageParts[0]
+		#For 'wikiarandom', retrieve a random page title
+		if message.trigger == 'wikiarandom':
+			success, result = self.getRandomWikiaArticleName(wikiName)
+			searchterm = result if success else None
+		#Otherwise, use the provided search term to look for a matching page title
+		else:
+			searchterm = " ".join(message.messageParts[1:])
+			success, result = self.searchForArticleTitle(wikiName, searchterm)
+
+		#If we found a title, use that to retrieve the actual article text
+		if success:
+			success, result = self.retrieveArticleAbstract(wikiName, result)
+		#Check if either call returned an error
 		if not success:
-			#Searching for the article went wrong, just say the error message
-			return message.reply(articleTitleOrError, "say")
-		#Found an article name, retrieve and say the article abstract (or the error message if something goes wrong)
-		message.reply(self.retrieveArticleAbstract(message.messageParts[0], articleTitleOrError)[1], "say")
+			result = self.errorCodeToMessage(result, wikiName, searchterm)
+		message.reply(result, "say")
 
 	@staticmethod
 	def isPageDisambiguationPage(url, title, pageText):
@@ -50,18 +67,62 @@ class Command(CommandTemplate):
 		return url.startswith("http://community.wikia.com/wiki/Community_Central:Not_a_valid_community?from=")
 
 	@staticmethod
+	def errorCodeToMessage(errorCode, wikiName, searchterm=None):
+		#Show an error message depending on the error returned
+		if errorCode == WikiaApiErrors.TIMEOUT:
+			return "Wikia apparently got confused, since it's taking ages to respond. Maybe try again in a bit?"
+		elif errorCode == WikiaApiErrors.INVALID_WIKIA_NAME:
+			return "Apparently the wiki '{}' doesn't exist on Wikia. You invented a new fandom!".format(wikiName)
+		elif errorCode == WikiaApiErrors.INVALID_API_CALL:
+			return "Huh, seems like something changed at Wikia, it doesn't seem to understand me. You should probably poke my owner(s) so they can look into that"
+		elif errorCode == WikiaApiErrors.API_DISABLED:
+			return "Aw, seems like the {0} Wikia disabled their API, so I can't look up info for you. You'll have to look it up yourself, sorry: https://{0}.wikia.com".format(wikiName)
+		elif errorCode == WikiaApiErrors.NO_RESULTS_FOUND:
+			if searchterm:
+				return "Apparently the page '{}' doesn't exist. Seems you know more about {} than the fandom! Or maybe you made a typo?".format(searchterm, wikiName)
+			else:
+				#We won't have a search term if 'wikiarandom' was used
+				return "Weirdly, the random page I thought I found doesn't seem to exist? I'm as confused as you are..."
+		return "Something went wrong with looking up '{}' on the {} Wikia, and I'm not sure what... Poke my owner(s) about this, see if they can figure out what's up".format(searchterm, wikiName)
+
+	@staticmethod
 	def retrieveApiResult(wikiName, apiUrl, params, timeout=10.0):
+		"""
+		Tries to retrieve data from the Wikia API
+		:param wikiName: The name of the Wikia wiki to get data from
+		:param apiUrl: The path to the specific API call
+		:param params: A dictionary of parameters to pass along to the API
+		:param timeout: A timeout in seconds for the API call
+		:return: A tuple. The first entry is a boolean, True if the call was successful, False if something went wrong.
+				If the call succeeded, the second entry is the dictionary the API call returned, otherwise it's an integer from the WikiaApiErrors enum class saying which error occurred
+		"""
 		try:
 			r = requests.get("http://{}.wikia.com/api/v1/{}".format(wikiName, apiUrl), timeout=10.0, params=params)
 		except requests.exceptions.Timeout:
-			return (False, "Wikia apparently got confused about that query, since it's taking ages. Maybe try again in a bit?")
+			return (False, WikiaApiErrors.TIMEOUT)
 
 		#If the wiki doesn't exist, we get redirected to a different page
 		if Command.isUrlInvalidWikiaPage(r.url):
-			return (False, "Apparently the wiki '{}' doesn't exist on Wikia. You invented a new fandom!".format(wikiName))
+			return (False, WikiaApiErrors.INVALID_WIKIA_NAME)
+
+		apireply = r.json()
+		#Check if the API returned results or an error
+		if 'exception' in apireply:
+			exceptionType = apireply['exception'].get('type', None)
+			if exceptionType == u"MethodNotFoundException":
+				#Check the type here instead of the code since it's a 404 too, just like NO_RESULTS_FOUND
+				return (False, WikiaApiErrors.INVALID_API_CALL)
+			exceptionCode = apireply['exception'].get('code', None)
+			if exceptionCode == 403:
+				return (False, WikiaApiErrors.API_DISABLED)
+			elif exceptionCode == 404:
+				return (False, WikiaApiErrors.NO_RESULTS_FOUND)
+			#If we reached here, it's an unknown error
+			Command.logWarning("[Wikia] An unknown exception was returned by the Wikia API. API result: " + json.dumps(apireply))
+			return (False, WikiaApiErrors.GENERIC_ERROR)
 
 		#Loading worked, return the API reply
-		return (True, r.json())
+		return (True, apireply)
 
 	@staticmethod
 	def searchForArticleTitle(wikiName, query):
@@ -69,11 +130,11 @@ class Command(CommandTemplate):
 		apiResultTuple = Command.retrieveApiResult(wikiName, "Search/List", {"query": query, "limit": "1"})
 		if not apiResultTuple[0]:
 			return apiResultTuple
-		apireply = apiResultTuple[1]
+		apireply = apiResultTuple[1]  # type: dict
 
-		#Check if no results were found
-		if 'items' not in apireply:
-			return (False, "The term '{}' doesn't seem to exist in the {} fandom. Time to write fanfic about it!".format(query, wikiName))
+		#If the requested page doesn't exist, the return is empty
+		if 'items' not in apireply or len(apireply['items']) == 0:
+			return (False, WikiaApiErrors.NO_RESULTS_FOUND)
 
 		#Found at least one article match, return the name of the top one
 		return (True, apireply['items'][0]['title'])
@@ -84,11 +145,11 @@ class Command(CommandTemplate):
 		apiResultTuple = Command.retrieveApiResult(wikiName, "Articles/Details", {"titles": articleName.replace(" ", "_"), "abstract": Constants.MAX_MESSAGE_LENGTH})
 		if not apiResultTuple[0]:
 			return apiResultTuple
-		apireply = apiResultTuple[1]
+		apireply = apiResultTuple[1]  # type: dict
 
 		#If the requested page doesn't exist, the return is empty
-		if len(apireply['items']) == 0:
-			return (False, "Apparently the page '{}' doesn't exist. Seems you know more about {} than the fandom. Or maybe you made a typo?".format(articleName, wikiName))
+		if 'items' not in apireply or len(apireply['items']) == 0:
+			return (False, WikiaApiErrors.NO_RESULTS_FOUND)
 
 		articleId = apireply['items'].keys()[0]
 		articleInfo = apireply['items'][articleId]
@@ -111,16 +172,14 @@ class Command(CommandTemplate):
 		return (True, articleAbstract + Constants.GREY_SEPARATOR + url)
 
 	@staticmethod
-	def getRandomWikiaArticle(wikiName):
+	def getRandomWikiaArticleName(wikiName):
 		try:
 			page = requests.get('http://{}.wikia.com/wiki/Special:Random'.format(wikiName), timeout=10.0)
 		except requests.exceptions.Timeout:
-			return (False, "Apparently Wikia couldn't pick between all of its interesting articles, so it took too long to reply. Sorry!")
+			return (False, WikiaApiErrors.TIMEOUT)
 
 		if Command.isUrlInvalidWikiaPage(page.url):
-			return (False, "That doesn't appear to be a valid Wikia wiki. Seems like you're good enough at making up random stuff that you don't need me!")
+			return (False, WikiaApiErrors.INVALID_WIKIA_NAME)
 
 		#Get the part of the URL that is the article title
-		articleName = page.url.split('/wiki/', 1)[1]
-		#And retrieve the abstract of the article in a more easily parsable format
-		return Command.retrieveArticleAbstract(wikiName, articleName)
+		return page.url.split('/wiki/', 1)[1]
