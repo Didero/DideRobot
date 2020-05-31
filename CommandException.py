6a09cc2e440f5677c4bec53ebbc6111b22213d15
Didero <Didero@users.noreply.github.com>
2020-05-31 20:31:12 +0200

    Add parameter saying whether the CommandException should be logged

--- CommandException.py
+++ CommandException.py
@@ -4,12 +4,14 @@ class CommandException(Exception):
 	The parameter is a message sent to the source that called the command (a channel or a user)
 	"""
 
-	def __init__(self, displayMessage=None):
+	def __init__(self, displayMessage=None, shouldLogError=True):
 		"""
 		Create a new CommandException, to be thrown when something goes wrong during Command execution
 		:param displayMessage: An optional message to display to the IRC chat the bot is in
+		:param shouldLogError: Whether this exception should be logged to the program log. This is useful if it's a problem that needs to be solved, but can be set to False if it's a user input error
 		"""
 		self.displayMessage = displayMessage
+		self.shouldLogError = shouldLogError
 
 	def __str__(self):
 		return self.displayMessage
