059cac70992c4b3ab7e4ce59fcaa2e78db5bb383
Didero <Didero@users.noreply.github.com>
2020-10-11 17:41:46 +0200

    Add CommandInputException
    This exception should be thrown on invalid user input. It won't get logged

--- CommandException.py
+++ CommandException.py
@@ -15,3 +15,17 @@ class CommandException(Exception):
 
 	def __str__(self):
 		return self.displayMessage
+
+
+class CommandInputException(CommandException):
+	"""
+	This custom exception can be raised when the input to some module or command is invalid or can't be parsed.
+	It is a more specific implementation of the CommandException, that doesn't log itself to the logfile
+	"""
+
+	def __init__(self, displayMessage):
+		"""
+		Create a new InputException. The display message will be shown to the user
+		:param displayMessage: The message to show to the user that called the command. This message should explain how the input should be correctly formatted
+		"""
+		super(CommandException, self).__init__(displayMessage, False)
