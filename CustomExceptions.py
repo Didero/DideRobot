2663ddc50405de8baf45fddab6ca414b124e758a
Didero <Didero@users.noreply.github.com>
2022-01-13 20:16:28 +0100

    Create a BaseCustomException
    In preparation for more specific custom exceptions

--- CustomExceptions.py
+++ CustomExceptions.py
@@ -1,20 +1,39 @@
-class CommandException(Exception):
+class BaseCustomException(Exception):
+	"""
+	An abstract class that other custom exceptions should inherit from. Don't instantiate this directly
+	"""
+	def __init__(self, displayMessage):
+		"""
+		Create a new exception
+		:param displayMessage: An optional user-facing message, that will also be used as the exception's string representation
+		"""
+		super(BaseCustomException, self).__init__(displayMessage)
+		self.displayMessage = displayMessage
+
+	def __str__(self):
+		if self.displayMessage:
+			return self.displayMessage
+		return super(BaseCustomException, self).__str__()
+
+	def __repr__(self):
+		if self.displayMessage:
+			return "<{}> {}".format(self.__class__.__name__, self.displayMessage)
+		return super(BaseCustomException, self).__repr__()
+
+
+class CommandException(BaseCustomException):
 	"""
 	This custom exception can be thrown by commands when something goes wrong during execution.
 	The parameter is a message sent to the source that called the command (a channel or a user)
 	"""
-
 	def __init__(self, displayMessage=None, shouldLogError=True):
 		"""
 		Create a new CommandException, to be thrown when something goes wrong during Command execution
 		:param displayMessage: An optional message to display to the IRC chat the bot is in
 		:param shouldLogError: Whether this exception should be logged to the program log. This is useful if it's a problem that needs to be solved, but can be set to False if it's a user input error
 		"""
-		self.displayMessage = displayMessage
+		super(CommandException, self).__init__(displayMessage)
 		self.shouldLogError = shouldLogError
-
-	def __str__(self):
-		return self.displayMessage
 
 
 class CommandInputException(CommandException):
