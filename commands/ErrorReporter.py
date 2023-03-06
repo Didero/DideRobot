3f056eb5336e10756f30c62102d972d65fe22856
Didero <Didero@users.noreply.github.com>
2023-03-06 21:15:00 +0100

    [ErrorReport] Add trigger to see warnings

--- commands/ErrorReporter.py
+++ commands/ErrorReporter.py
@@ -7,8 +7,8 @@ from util import StringUtil
 
 
 class Command(CommandTemplate):
-	triggers = ['errorreport']
-	helptext = "Looks through the bot's logs and reports any errors it finds. Admin-only, and only works in private messages to prevent spam"
+	triggers = ['errorreport', 'warningreport']
+	helptext = "Looks through the bot's logs and reports any errors or warnings (depending on the trigger) it finds. Admin-only, and only works in private messages to prevent spam"
 
 	def execute(self, message):
 		"""
@@ -20,33 +20,36 @@ class Command(CommandTemplate):
 		if not message.isPrivateMessage:
 			return message.reply("Sorry, this command only works in private messages, to prevent spam and possibly showing private information")
 
-		# Go through all the Program.log files to look for errors
+		logLevel = 'error' if message.trigger == 'errorreport' else 'warning'
+
+		# Go through all the Program.log files to look for lines with the requested loglevel
 		# Start with any Program.log.[date] files, and then Program.log itself, to keep things chronological
-		errors = []
+		matchingLines = []
 		numberOfFilesChecked = 0
 		for fn in os.listdir(GlobalStore.scriptfolder):
 			if fn.startswith('Program.log.'):
-				errors.extend(self.findErrorsInLogFile(os.path.join(GlobalStore.scriptfolder, fn)))
+				matchingLines.extend(self.findLogLevelLinesInLogfile(os.path.join(GlobalStore.scriptfolder, fn), logLevel))
 				numberOfFilesChecked += 1
 		if os.path.isfile(os.path.join(GlobalStore.scriptfolder, 'Program.log')):
-			errors.extend(self.findErrorsInLogFile(os.path.join(GlobalStore.scriptfolder, 'Program.log')))
+			matchingLines.extend(self.findLogLevelLinesInLogfile(os.path.join(GlobalStore.scriptfolder, 'Program.log'), logLevel))
 			numberOfFilesChecked += 1
 
-		if not errors:
-			message.reply("Hurray, no errors were found in the {:,} logfiles I checked. That's because I'm programmed incredibly wlel"  # The typo in 'wlel' is intentional, it's a joke
-						  .format(numberOfFilesChecked), 'say')
+		if not matchingLines:
+			message.reply("Hurray, no {}s were found in the {:,} logfiles I checked. That's because I'm programmed incredibly wlel"  # The typo in 'wlel' is intentional, it's a joke
+						  .format(logLevel, numberOfFilesChecked), 'say')
 		else:
-			message.reply("Found {:,} error{} in {:,} logfile{}:".format(len(errors), '' if len(errors) == 1 else 's', numberOfFilesChecked, '' if numberOfFilesChecked == 1 else 's'))
-			for error in errors:
-				message.reply(StringUtil.limitStringLength(error, Constants.MAX_MESSAGE_LENGTH), 'say')
+			message.reply("Found {:,} {}{} in {:,} logfile{}:".format(len(matchingLines), logLevel, '' if len(matchingLines) == 1 else 's', numberOfFilesChecked, '' if numberOfFilesChecked == 1 else 's'))
+			for line in matchingLines:
+				message.reply(StringUtil.limitStringLength(line, Constants.MAX_MESSAGE_LENGTH), 'say')
 
-	def findErrorsInLogFile(self, logFilePath):
-		errors = []
+	def findLogLevelLinesInLogfile(self, logFilePath, logLevelString):
+		matchingLines = []
+		logLevelStringToFind = '({})'.format(logLevelString.upper())
 		with codecs.open(logFilePath, 'r', 'utf-8') as logFile:
 			for line in logFile:
-				if '(ERROR)' in line:
-					# Check if it is an actual error entry and not just a line that happens to have '(ERROR)' somewhere in it
+				if logLevelStringToFind in line:
+					# Check if it is an actual matching entry and not just a line that happens to have the log level text somewhere in it
 					lineParts = line.split(' ', 3)
-					if len(lineParts) > 3 and lineParts[2] == '(ERROR)':
-						errors.append(line)
-		return errors
+					if len(lineParts) > 3 and lineParts[2] == logLevelStringToFind:
+						matchingLines.append(line)
+		return matchingLines
