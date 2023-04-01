68715f57499e448db8e1e0daf42d803656497de4
Didero <Didero@users.noreply.github.com>
2023-04-01 21:25:14 +0200

    Increase line and message length limits
    They were too conservative
    
    Also add calculation for message length limit

--- Constants.py
+++ Constants.py
@@ -1,6 +1,6 @@
 CTCP_DELIMITER = chr(1)
-MAX_LINE_LENGTH = 450  #Officially 512 bytes including the newline characters, but let's be on the safe side
-MAX_MESSAGE_LENGTH = 300  #The maximum number of characters in message to send. Less than max line length because that includes channel and nickname
+MAX_LINE_LENGTH = 510  # Officially 512 bytes, but lines always end with '\r\n', so subtract 2
+MAX_MESSAGE_LENGTH = 325  # The maximum number of characters in message to send. Message format is ':[nick,32]![username,12]@[hostmask,64] PRIVMSG [target,64] :[messagetext]', which leaves 326 characters for messagetext, round it down a bit to be safe
 CHANNEL_PREFIXES = "#&!+.~"  #All the characters that could possibly indicate something is a channel name (usually just '#' though)
 #Since a grey separator is often used to separate parts of a message, provide an easy way to get one
 GREY_SEPARATOR = u' \x0314|\x03 '  #'\x03' is the 'color' control char, 14 is the colour code for grey
