c4cefe217ab47842f562b07f36ffa4094e705653
Didero <Didero@users.noreply.github.com>
2023-05-18 14:41:05 +0200

    [UrlTitleFinder] Add Tumblr URL handling
    This requires a Tumblr API key

--- commands/UrlTitleFinder.py
+++ commands/UrlTitleFinder.py
@@ -1,4 +1,4 @@
-import html, re
+import html, json, re
 
 import requests
 
@@ -38,8 +38,8 @@ class Command(CommandTemplate):
 			for url in urlmatches:
 				# Go through the methods alphabetically, and use the generic method last
 				title = None
-				for parseMethod in (self.retrieveImgurTitle, self.retrieveMastodonTitle, self.retrieveSteamTitle, self.retrieveTwitchTitle, self.retrieveTwitterTitle,
-									self.retrieveWikipediaTitle, self.retrieveYoutubeTitle, self.retrieveGenericTitle):
+				for parseMethod in (self.retrieveImgurTitle, self.retrieveMastodonTitle, self.retrieveSteamTitle, self.retrieveTumblrTitle, self.retrieveTwitchTitle,
+									self.retrieveTwitterTitle, self.retrieveWikipediaTitle, self.retrieveYoutubeTitle, self.retrieveGenericTitle):
 					try:
 						title = parseMethod(url)
 					except requests.exceptions.Timeout:
@@ -188,3 +188,49 @@ class Command(CommandTemplate):
 		if not urlMatch:
 			return None
 		return GlobalStore.commandhandler.runCommandFunction('getSteamAppDescriptionById', None, urlMatch.group('appid'), False)
+
+	@staticmethod
+	def retrieveTumblrTitle(url):
+		urlMatch = re.match('https://(?:www\.)?tumblr.com/(?P<user>[^/]+)/(?P<postId>\d+)', url, re.IGNORECASE)
+		if not urlMatch:
+			return None
+		if 'tumblr' not in GlobalStore.commandhandler.apikeys:
+			Command.logWarning("[UrlTitleFinder] No Tumblr API key stored")
+			return None
+		apiReply = requests.get("https://api.tumblr.com/v2/blog/{}/posts".format(urlMatch.group('user')), params={'api_key': GlobalStore.commandhandler.apikeys['tumblr'], 'id': urlMatch.group('postId'), 'filter': 'text', 'npf': True})
+		if apiReply.status_code != 200:
+			Command.logError("[UrlTitleFinder] Tumblr API returned error result, status code {}: {}".format(apiReply.status_code, apiReply.text))
+			return None
+		try:
+			postData = apiReply.json()['response']['posts']
+		except json.JSONDecodeError as e:
+			Command.logError(f"[UrlTitleFinder] Unable to parse API reply as JSON: {e}; API reply: {apiReply.text}")
+			postData = None
+		if not postData:
+			# No posts found, apparently
+			return None
+		# We only care about the first post, which should also be the only post
+		postData = postData[0]
+
+		# If the 'trail' entry is filled, it's a reblog. The 'trail' dict contains all the info we need of the original post, so use that instead of the toplevel data
+		if 'trail' in postData and postData['trail']:
+			postData = postData['trail'][0]
+
+		# Format the post text
+		postText = None
+		if 'content' in postData:
+			# Start with the poster's name
+			postTextParts = []
+			# This list contains dicts with the type and content of each entry. Only parse the text entries
+			for contentEntry in postData['content']:
+				if contentEntry['type'] == 'text' and 'text' in contentEntry:
+					postTextParts.append(contentEntry['text'].strip())
+			if postTextParts:
+				postText = IrcFormattingUtil.makeTextBold(postData['blog']['name']) + ': ' + Constants.GREY_SEPARATOR.join(postTextParts)
+		elif 'summary' in postData:
+			postText = postData['summary']
+		else:
+			Command.logWarning(f"[UrlTitleFinder] No usable data found in Tumblr API reply for url '{url}': {apiReply.json()}")
+
+		# Send the result, if any
+		return StringUtil.removeNewlines(postText) if postText else None
