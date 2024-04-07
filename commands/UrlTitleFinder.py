1013481cad4603a4ae8982d02ebf0baf7c0de0c9
Didero <Didero@users.noreply.github.com>
2024-04-07 11:48:29 +0200

    [UrlTitleFinder] Try to get English response

--- commands/UrlTitleFinder.py
+++ commands/UrlTitleFinder.py
@@ -66,7 +66,7 @@ class Command(CommandTemplate):
 		# Only parse text documents, and not images or videos or the like. Retrieve the url header to check the content type
 		# We need to add a User-Agent, otherwise the request gets discarded by some sites
 		try:
-			headersResponse = requests.head(url, headers={'User-Agent': 'DideRobot'}, allow_redirects=True, timeout=Command.lookupTimeoutSeconds)
+			headersResponse = requests.head(url, headers={'User-Agent': 'DideRobot', 'Accept-Language': 'en'}, allow_redirects=True, timeout=Command.lookupTimeoutSeconds)
 			# Some sites return a 405 or similar error code while still filling in the header, so ignore known 'lying' status codes
 			if headersResponse.status_code != 200 and headersResponse.status_code != 405:
 				return None
