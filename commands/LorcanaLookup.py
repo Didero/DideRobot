692cebc9348810f3c6cf62057bfee21d9016627c
Didero <Didero@users.noreply.github.com>
2024-05-19 11:56:40 +0200

    [LorcanaLookup] Also stringify (non)enchanted and (non)promo IDs

--- commands/LorcanaLookup.py
+++ commands/LorcanaLookup.py
@@ -214,11 +214,16 @@ class Command(CommandTemplate):
 		# We're going to be searching the cards from the end to front of the list, for efficiency. Since promo cards have higher IDs, reverse the list so the 'normal' versions come first
 		cardData['cards'].reverse()
 		# Store boolean and numerical card values as strings, to match regexes easier while searching
-		fieldNamesToStringify = ('cost', 'id', 'inkwell', 'lore', 'moveCost', 'number', 'setNumber', 'strength', 'willpower')
+		fieldNamesToStringify = ('cost', 'enchantedId', 'id', 'inkwell', 'lore', 'moveCost', 'nonEnchantedId', 'nonPromoId', 'number', 'promoIds', 'setNumber', 'strength', 'willpower')
 		for card in cardData['cards']:
 			for fieldName in fieldNamesToStringify:
 				if fieldName in card:
-					card[fieldName] = str(card[fieldName])
+					if isinstance(card[fieldName], int):
+						card[fieldName] = str(card[fieldName])
+					elif isinstance(card[fieldName], list):
+						card[fieldName] = [str(fieldValue) for fieldValue in card[fieldName]]
+					else:
+						self.logError(f"[LorcanaLookup] Encountered unsupported field type '{type(card[fieldName])}' for field '{fieldName}' while updating")
 		with open(self.CARD_FILE_PATH, "w", encoding="utf-8") as cardFile:
 			json.dump(cardData, cardFile)
 		with open(self.VERSION_FILE_PATH, "w", encoding="utf-8") as versionFile:
